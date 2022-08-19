4c4c513833e7e4757d851a56c53003ecf3bd0881
florishi <flroisihi12@gmail.com>
2022-08-19 09:57:29 -0300

    Adicionando rotas

---
+++ api/rota/usuario.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var express = require('express');
+var UsuarioControle = (controle / usuario);
+
+var api = express.Router();
+
+api.get('home', UsuarioControle.home);
+api.get('test', UsuarioControle.test);
+
+module.exports = api;
